a7da1598041361a89140fed61d8d29c9be8a21f6
EMGS <getahuntamirat08@gmail.com>
2025-06-13 15:23:37 -0700

    update setting

--- Emgs/urls.py
+++ Emgs/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from knox import views as knox_views
+from django_rest_passwordreset.views import ResetPasswordRequestToken, ResetPasswordConfirm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +10,6 @@ urlpatterns = [
     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
     path('api/password_reset/', include('django_rest_passwordreset.urls',namespace='password_rest')), 
-    
+    path('api/password_reset/confirm/', ResetPasswordConfirm.as_view(), name='password_reset_confirm'),
+
 ]
