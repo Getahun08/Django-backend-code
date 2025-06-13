d1f62623860158dddc613dd1f658c94c03a6c244
EMGS <getahuntamirat08@gmail.com>
2025-06-13 09:11:52 -0700

    update build.sh for superuser

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-
-if [[$CREATE_SUPERUSER]];
-then
-    python manage.py createsuperuser --email getu@gmail.com --password getu1234
-
-fi    
-
 python manage.py migrate
+if [ "$CREATE_SUPERUSER" = "True" ]; then
+    python manage.py createsuperuser --no-input
+fi
