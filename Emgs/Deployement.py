83e58e36c714168a4a6db8cf0c21a4363b3492b6
EMGS <getahuntamirat08@gmail.com>
2025-06-13 12:27:53 -0700

    update Deployemet

--- Emgs/Deployement.py
+++ Emgs/Deployement.py
@@ -3,10 +3,11 @@ from.settings import *
 import dj_database_url
 from settings import BASE_DIR
 
-ALLOWED_HOSTS=[os.environ.get('WEPSITE_HOSTNAME')]
-CSRF_TRESTED_ORGINS=['https://'+os.environ.get('WEPSITE_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', 'django-backend-code.onrender.com')]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('WEBSITE_HOSTNAME', 'django-backend-code.onrender.com')]
+
 DEBUG=False
-SECRET_KEY=os.environ('MY_SECRET_KEY')
+SECRET_KEY = os.environ.get('MY_SECRET_KEY', 'fallback-secret-key')
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
@@ -19,7 +20,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_auto_logout.middleware.auto_logout'
 ]
-CORS_ALLOWED_ORIGINS = [ 'https://employe-management-sysytem.onrender.com']
+CORS_ALLOWED_ORIGINS = [
+    "https://employe-management-sysytem.onrender.com",
+    "https://django-backend-code.onrender.com"
+]
 
 STORAGES={'default': 
           {'BACKEND': 'django.core.files.storage.FileSystemStorage'},
@@ -27,15 +31,19 @@ STORAGES={'default':
            'staticfiles':
              {'BACKEND': "whitenoise.storage.CompressedStaticFilesStorage"},
                }
-# CONNECTION=os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-# CONNECTION_STR={pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
-DATABASES = {
-   'default': {
-       'ENGINE': dj_database_url.config(
-           default=os.environ['DATABASE_URL'],
-           conn_max_age=600
+# DATABASES = {
+#    'default': {
+#        'ENGINE': dj_database_url.config(
+#            default=os.environ['DATABASE_URL'],
+#            conn_max_age=600
 
-       )
+#        )
        
-   }
-} 
+#    }
+# } 
+DATABASES = {
+   'default': dj_database_url.config(
+       default=os.environ.get('DATABASE_URL'),
+       conn_max_age=600
+   )
+}
